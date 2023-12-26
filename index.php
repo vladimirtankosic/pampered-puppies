fa74db37dd2114260630970740a434b42b8e2855
vladimirtankosic <89266262+vladimirtankosic@users.noreply.github.com>
2023-12-26 16:48:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once "pages/head.php" ?>
+
+  <body>
+
+    <?php require_once "pages/nav.php" ?>
+    <article class="page">
+    <?php require_once "pages/home.php" ?>
+
+    <?php require_once "pages/about-us.php" ?>
+
+    <?php require_once "pages/litters.php" ?>
+
+    <?php require_once "pages/parents.php" ?>
+
+    <?php require_once "pages/gallery.php" ?>
+
+    <?php require_once "pages/contact.php" ?>
+    </article>
+
+    <?php require_once "pages/footer.php" ?>
+
+    <script src="js/swiper-bundle.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
